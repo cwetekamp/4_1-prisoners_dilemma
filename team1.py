b8800f837c35b23bccedc454411438c39f685ead
MFisher1490 <matthew.fisher@students.sacs.k12.in.us>
2016-12-14 10:55:48 -0500

    Second Change
    Change 2

---
+++ team1.py
@@ -0,0 +1,7 @@
+# three strings team_name, strategy_name, and strategy_description
+# and a function move(my_history, their_history, my_score, their_score)
+team_name = Win
+strategy_name = winning
+strategy_description = wewillwin
+def move(my_history, their_history, my_score, their_score):
+    
